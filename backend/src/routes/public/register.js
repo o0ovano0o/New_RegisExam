326695364eeaf5d81ebbb87393664983f70a5b9e
anhntv <anhntv@kikaitech.io>
2020-12-11 22:29:47 +0700

    sửa api register tích hợp

--- backend/src/routes/public/register.js
+++ backend/src/routes/public/register.js
@@ -41,16 +41,33 @@ router.post('/api/student/register', async (req, res) => {
   }
 });
 
+// router.post('/intergrates/register', async (req, res) => {
+//   try {
+//     const { userName , name ,email , dateOfBirth , address  } = req.body;
+//     if (!userName || !name) {
+//       return res.status(400).json({ success: false, msg: 'Thiếu thông tin bắt buộc' });
+//     }
+
+//     const rows = await knex('student').where({ studentcode:userName }).count('*', { as: 'count' });
+//     if (rows[0].count > 0) return res.status(400).json({ success: false, msg: 'Tên tài khoản đã tồn tại' });
+//     const result = await knex('student').insert({ studentcode: userName , fullname: name, password: sha1(userName), datebirth: dateOfBirth, hometown:address, class: req.body.class, email: email });
+//     if (!result) return res.status(400).json({ success: true, msg: 'Đăng ký tài khoản thất bại' });
+//     return res.status(200).json({ success: true, msg: 'Đăng ký tài khoản thành công' });
+//   } catch (err) {
+//     handleAPIError(err, res);
+//   }
+// });
+
 router.post('/intergrates/register', async (req, res) => {
   try {
-    const { userName , name ,email , dateOfBirth , address   } = req.body;
-    if (!userName || !name) {
+    const { UserName , Name , Email , DateOfBirth , Address  } = req.body;
+    if (!UserName || !Name) {
       return res.status(400).json({ success: false, msg: 'Thiếu thông tin bắt buộc' });
     }
 
-    const rows = await knex('student').where({ studentcode:userName }).count('*', { as: 'count' });
+    const rows = await knex('student').where({ studentcode:UserName }).count('*', { as: 'count' });
     if (rows[0].count > 0) return res.status(400).json({ success: false, msg: 'Tên tài khoản đã tồn tại' });
-    const result = await knex('student').insert({ studentcode: userName , fullname: name, password: sha1(userName), datebirth: dateOfBirth, hometown:address, class: req.body.class, email: email });
+    const result = await knex('student').insert({ studentcode: UserName , fullname: Name, password: sha1(UserName), datebirth: DateOfBirth, hometown:Address, class: req.body.Class, email: Email });
     if (!result) return res.status(400).json({ success: true, msg: 'Đăng ký tài khoản thất bại' });
     return res.status(200).json({ success: true, msg: 'Đăng ký tài khoản thành công' });
   } catch (err) {
